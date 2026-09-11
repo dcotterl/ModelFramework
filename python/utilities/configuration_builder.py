c2e88a9a87a695a1117844e9a433e9ecb1c64f70
DavideCotterle <davide.cotterle@emerson.com>
2026-09-11 16:09:56 +0200

    added example to create mapping

--- python/utilities/configuration_builder.py
+++ python/utilities/configuration_builder.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 from niveristand.systemdefinitionapi import SystemDefinition, Model
 from pathlib import Path
+import mapping
+import subsystems
 
-COMPILED_MODEL_FOLDER = "C:\\Users\\VECU\\Documents\\ModelFramework\\matlab_simulink"
+TARGET_MODEL = "matlab_simulink\\model\\average_subsystems_2025b.slx"
 TARGET_VERISTAND_PATH = "veristand\\workflow_example\\workflow_example.nivssdf"
-
-DEFAULT_MODEL = (
+DEFAULT_MODEL_FOLDER = (
 	Path(__file__).parents[2]
 	/ "matlab_simulink"
 )
+DEFAULT_TARGET_NAME = "Controller"
 
-def create_configuration(target_type : str = "Windows", target_ip : str = "127.0.0.1", output_path=TARGET_VERISTAND_PATH):
+def create_configuration(target_name : str = DEFAULT_TARGET_NAME, target_type : str = "Windows", target_ip : str = "localhost", output_path=TARGET_VERISTAND_PATH):
 	"""Create a VeriStand system definition and return it."""
 
 	system_definition = SystemDefinition()
@@ -21,13 +23,14 @@ def create_configuration(target_type : str = "Windows", target_ip : str = "127.0
 		"System Definition created by ModelFramework",
 		"ModelFramework",
 		"1.0.0.0",
-		"Controller",
+		target_name,
 		target_type,
 		str(filepath),
 	)
 
 	target = system_definition.root.get_targets().get_target_list()[0]
 	target.ip_address = target_ip
+	target.target_rate = 1000
 
 	return system_definition
 
@@ -55,24 +58,24 @@ def add_model(system_definition, model, target_name=None):
 	simulation_models.get_models().add_model(model)
 	return system_definition
 
-
-def main():
+def create_configuration_with_compiled_models(): # show how to generate a VeriStand system definition with compiled models
 	config = create_configuration()
-	models = get_compiled_models(COMPILED_MODEL_FOLDER)
-	models = get_compiled_models(DEFAULT_MODEL)
+	models = get_compiled_models(DEFAULT_MODEL_FOLDER)
 	for model in models:
 		model = Model(model.stem,
 					  f"Implementation of {model.stem}{model.suffix} from simulink", 
 					  str(model),
 					  0, 1, 0, True, True, True)
-		config = add_model(config, model,"Controller")
-
-
-
+		config = add_model(config, model, DEFAULT_TARGET_NAME)
 	save_configuration(config)
 
+def create_mapping():
+	mapping.create_mapping_file(subsystems.map_goto_from_connections(TARGET_MODEL), 
+							    DEFAULT_TARGET_NAME, 
+								mapping.TARGET_MAPPING_FILE)
 
 if __name__ == "__main__":
 
-	main()
+	create_configuration_with_compiled_models()
+	#create_mapping()
 
--- python/utilities/mapping.py
+++ python/utilities/mapping.py
@@ -1,9 +1,9 @@
 import subsystems
-from pathlib import Path
 
 TEST_MODEL_PATH = "matlab_simulink\\model\\average_subsystems_2025b.slx"
+TARGET_MAPPING_FILE = "veristand\\generated_mapping.txt"
 
-def wire_models(model_map, controller_name):
+def assign_links(model_map, controller_name):
     mapping = []
     for entry in model_map:
         from_string = f"Targets/{controller_name}/Simulation Models/Models/{entry['sources'][0]['subsystem']}/Outports/{entry['sources'][0]['port']}"
@@ -12,8 +12,12 @@ def wire_models(model_map, controller_name):
             mapping.append(f"{from_string}\t{to_string}")
     return mapping
 
+def create_mapping_file(model_map, controller_name, output_path):
+    mapping = assign_links(model_map, controller_name)
+    with open(output_path, "w") as f:
+        for entry in mapping:
+            f.write(f"{entry}\n")
+
 if __name__ == "__main__":
     map = subsystems.map_goto_from_connections(TEST_MODEL_PATH)
-    mapping = wire_models(map, "Controller")
-    for entry in mapping:
-        print(entry)
+    create_mapping_file(map, "Controller", TARGET_MAPPING_FILE)
